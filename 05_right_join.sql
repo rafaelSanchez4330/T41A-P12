1b44476be84196b53fd3f61734bf0abc70abdb55
Rafael Sánchez Saucedo <rafaelsanchez9924@gmail.com>
2025-11-19 19:28:45 -0600

    Implement RIGHT JOIN to fetch orders with customers

--- 05_right_join.sql
+++ 05_right_join.sql
@@ -1,2 +1,4 @@
 -- RIGHT JOIN: todos los pedidos, aunque no tengan cliente
-SELECT * FROM orders;
+SELECT cus.name, ord.product
+FROM customers AS cus
+RIGHT JOIN orders AS ord ON ord.customer_id = cus.id ORDER BY ord.id, cus.name;
