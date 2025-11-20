cbeec480227785b616d1a0373d027178091e76f7
Rafael Sánchez Saucedo <rafaelsanchez9924@gmail.com>
2025-11-19 19:22:31 -0600

    Add INNER JOIN query for customers and orders
    Added a query to perform an INNER JOIN between customers and orders.

--- 03_inner_join.sql
+++ 03_inner_join.sql
@@ -1,4 +1,6 @@
 
 -- INNER JOIN: solo coincidencias
 SELECT * FROM customers;
-
+SELECT cus.name, ord.product
+FROM customers AS cus
+INNER JOIN orders AS ord ON ord.customer_id = cus.id ORDER BY cus.name, ord.id;
