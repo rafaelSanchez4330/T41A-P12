0f2ccb705afb7a361bd9591476ece66db5acb455
Rafael Sánchez Saucedo <rafaelsanchez9924@gmail.com>
2025-11-19 19:39:42 -0600

    Update 06_full_outer_join.sql

--- 06_full_outer_join.sql
+++ 06_full_outer_join.sql
@@ -1,2 +1,4 @@
 -- FULL OUTER JOIN: todos los clientes y pedido
-SELECT * FROM customers;
+SELECT cus.name, ord.product
+FROM customers AS cus
+FULL OUTER JOIN orders AS ord ON ord.customer_id = cus.id ORDER BY cus.name NULLS LAST, ord.id;
