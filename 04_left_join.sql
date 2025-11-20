df6c68170834559af43cbb05570b178d4766691f
Rafael Sánchez Saucedo <rafaelsanchez9924@gmail.com>
2025-11-19 19:27:22 -0600

    Add LEFT JOIN query to retrieve customer orders

--- 04_left_join.sql
+++ 04_left_join.sql
@@ -1,2 +1,5 @@
 -- LEFT JOIN: todos los clientes, aunque no tengan pedidos
-SELECT * FROM orders;
+
+SELECT cus.name, ord.product
+FROM customers AS cus
+LEFT JOIN orders AS ord ON ord.customer_id = cus.id ORDER BY cus.name, ord.id;
